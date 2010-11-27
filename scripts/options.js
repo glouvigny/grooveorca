cb09d94566ed100e37a733af1d512f7759054196
Guillaume L <glouvigny@users.noreply.github.com>
2010-11-27 15:12:12 +0100

    Version 0.7.3 - First commit

---
+++ scripts/options.js
@@ -0,0 +1,40 @@
+var options = {
+	switchView: function() {
+		view = this.id.slice(0,-5);
+		var views = document.getElementById("page").childNodes;
+		for(var i = 0; i < views.length; i++ ) {
+			if(views[i].nodeType == 1) {
+				if(views[i].nodeName == "DIV" && views[i].id != view) {
+					views[i].style.display = 'none';
+				} else {
+					views[i].style.display = 'block';
+				}
+			}
+		}
+	},
+	init: function(source) {
+		lastfm.init('options');
+		notifications.init('options');
+		wikipedia.init('options');
+		interfaceTweaks.init('options');
+		nowplaying.init('options');
+		
+		document.getElementById('LastFmLogin').addEventListener('click',lastfm.getAuth,true);
+		document.getElementById('LastFmVerifyLogin').addEventListener('click',lastfm.getSession,true);
+		document.getElementById('LastFmLogout').addEventListener('click',lastfm.destroySession,true);
+		document.getElementById('WikipediaLocale').addEventListener('input',wikipedia.changeLocale,true);
+		document.getElementById('notificationsDuration').addEventListener('input',notifications.toggleStatus,true);
+		
+		document.getElementById('activateScrobbling').addEventListener('change',lastfm.toggleStatus,true);
+		document.getElementById('activateNotifications').addEventListener('change',notifications.toggleStatus,true);
+		document.getElementById('activateInterfacesBlockTopBar').addEventListener('change',interfaceTweaks.toggleStatus,true);
+		document.getElementById('activateInterfacseBlockFooter').addEventListener('change',interfaceTweaks.toggleStatus,true);
+		document.getElementById('activateNowPlaying').addEventListener('change',nowplaying.toggleStatus,true);
+		
+		document.getElementById('lastfm-menu').addEventListener('click',options.switchView,true);
+		document.getElementById('notifications-menu').addEventListener('click',options.switchView,true);
+		document.getElementById('wikipedia-menu').addEventListener('click',options.switchView,true);
+		document.getElementById('interface-menu').addEventListener('click',options.switchView,true);
+		document.getElementById('wlm-menu').addEventListener('click',options.switchView,true);
+	}
+}
---
+++ scripts/page.js
@@ -0,0 +1,162 @@
+// Element dom pour récupérer plus de données. 
+var deezerPlusDom = document.createElement('div');
+deezerPlusDom.id = "deezerPlus";
+deezerPlusDom.style.display = 'none';
+var lastUpdateDom = document.createElement('div');
+lastUpdateDom.id = "lastUpdate"
+deezerPlusDom.appendChild(lastUpdateDom);
+document.getElementById('body').appendChild(deezerPlusDom);
+
+var DeezerPlus = {
+	deezerPage: null,
+	songInf: {
+		paused: null,
+		currentSong: null,
+		currentArtist: null,
+		currentAlbum: null,
+		currentDuration: null,
+		currentPosition: null,
+		currentVolume:null,
+		lastUpdate:null,
+		firstSeen:null,
+		firstSeenPlaying:null,
+		coverURL:""
+	},
+	getInformations: function() {
+		// !Album, Artiste, Piste = nouvelle piste, on note la date de 1ère apparition
+		if(DeezerPlus.songInf.currentArtist != document.getElementById('deezerPlus').getAttribute('artist') ||
+			DeezerPlus.songInf.currentAlbum != document.getElementById('deezerPlus').getAttribute('album') || 
+			DeezerPlus.songInf.currentSong != document.getElementById('deezerPlus').getAttribute('track')) {
+			
+			DeezerPlus.songInf.currentArtist	= document.getElementById('deezerPlus').getAttribute('artist');
+			DeezerPlus.songInf.currentAlbum 	= document.getElementById('deezerPlus').getAttribute('album');
+			DeezerPlus.songInf.currentSong 		= document.getElementById('deezerPlus').getAttribute('track');
+			
+			// Evite les scrobbles chiants.
+			DeezerPlus.songInf.currentPosition 	= 0;
+			
+			DeezerPlus.songInf.firstSeen = Math.floor(new Date().getTime()/1000);
+			DeezerPlus.songInf.firstSeenPlaying = null;
+			
+			// On push pas, certaines infos seront peut-être modifiées de nouveau
+			return false;
+		}		
+		DeezerPlus.songInf.coverURL			= document.getElementById('deezerPlus').getAttribute('coverurl');
+		DeezerPlus.songInf.currentDuration	= document.getElementById('deezerPlus').getAttribute('duration');
+		DeezerPlus.songInf.currentPosition	= document.getElementById('deezerPlus').getAttribute('position');
+		
+		if(document.getElementById('deezerPlus').getAttribute('playing') == 'false')
+			DeezerPlus.songInf.paused = true;
+		else
+			DeezerPlus.songInf.paused = false;
+		DeezerPlus.songInf.currentVolume	= document.getElementById('deezerPlus').getAttribute('volume');
+		DeezerPlus.songInf.lastUpdate 		= Math.floor(new Date().getTime()/1000);
+		
+		// Piste en lecture récup. de l'heure de début
+		if((DeezerPlus.songInf.firstSeenPlaying == null && !isNaN(DeezerPlus.songInf.currentPosition) && DeezerPlus.songInf.currentPosition != 0))
+			DeezerPlus.songInf.firstSeenPlaying = Math.floor(new Date().getTime()/1000);
+		
+		chrome.extension.sendRequest({name: "pushInfos", content: DeezerPlus.songInf}, function(response) { return true; });
+		chrome.extension.sendRequest({name: "updatePopup", content: false}, function(response) { return true; });
+		
+		return true;
+	},		
+	onDomUnload: function(e) {
+		chrome.extension.sendRequest({name: "unload"}, function(response) { return true; });
+	},
+	onDomLoad: function(e) {
+		if(document.getElementById('artName') != null) {
+			if(document.getElementById('songTitle') != null) {
+			
+			 // Fonction injectée via un location.href chargée de mettre les infos dans deezerPlus.
+			 location.href = "javascript:" +
+				"function updateDeezerPlus() {" +
+					"if((parseInt(document.getElementById('lastUpdate').getAttribute('updated')) + 100 < Math.floor(new Date().getTime())) || document.getElementById('lastUpdate').innerHTML == '') {" +
+						"document.getElementById('deezerPlus').setAttribute('playing',dzPlayer.isPlaying());" +
+						"document.getElementById('deezerPlus').setAttribute('coverurl',dzPlayer.getCoverURL());" +
+						"document.getElementById('deezerPlus').setAttribute('artist',dzPlayer.getArtistName());" +
+						"document.getElementById('deezerPlus').setAttribute('shuffle',dzPlayer.isShuffle());" +
+						"document.getElementById('deezerPlus').setAttribute('position',dzPlayer.getPosition());" +
+						"document.getElementById('deezerPlus').setAttribute('track',dzPlayer.getSongTitle());" +
+						"document.getElementById('deezerPlus').setAttribute('duration',dzPlayer.getDuration());" +
+						"document.getElementById('deezerPlus').setAttribute('album',dzPlayer.getAlbumTitle());" +
+						"document.getElementById('deezerPlus').setAttribute('paused',dzPlayer.isPaused());" +
+						"document.getElementById('deezerPlus').setAttribute('volume',dzPlayer.getVolume());" +
+						"document.getElementById('lastUpdate').removeAttribute('updated');" +
+						"document.getElementById('lastUpdate').setAttribute('updated',Math.floor(new Date().getTime()));" +
+					"};" +
+				"};" +
+				"document.getElementById('songPosition').addEventListener('DOMSubtreeModified', updateDeezerPlus , false);" +
+				"document.getElementById('play').addEventListener('DOMSubtreeModified', updateDeezerPlus , false);" +
+				"document.getElementById('pause').addEventListener('DOMSubtreeModified', updateDeezerPlus , false);" +
+				"document.getElementById('volumeslider').addEventListener('DOMSubtreeModified', updateDeezerPlus , false);" +
+				"document.getElementById('random').addEventListener('DOMSubtreeModified', updateDeezerPlus , false);";
+				document.getElementById('lastUpdate').addEventListener('DOMSubtreeModified', DeezerPlus.getInformations , false);
+				window.onbeforeunload = function() {
+					DeezerPlus.onDomUnload();
+				};
+			}
+		}
+	},
+	getUsername: function() {
+		var username;
+		var tmp = document.getElementsByTagName('DIV');
+		for(var i = 0; i < tmp.length; i++) {
+			if(tmp[i].getAttribute('class') == 'username') {
+				username = tmp[i].innerText;
+				break;
+			}
+		}
+	return username;
+	}
+}
+
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+	 switch (request.name) {
+           case "controlDeezer":
+				switch(request.action) {
+					case "prev":
+						location.href = "javascript:playercontrol.doAction('prev');";
+					break;
+					case "play":
+						location.href = "javascript:playercontrol.doAction('play');";
+					break;
+					case "pause":
+						location.href = "javascript:playercontrol.doAction('pause');";
+					break;
+					case "next":
+						location.href = "javascript:playercontrol.doAction('next');";
+					break;
+					case "login":
+						location.href = "javascript:loadBox('login.php', 'loading', 'general', '', '0', '0', '');";
+					break;
+					case "register":
+						location.href = "javascript:loadBox('register.php', 'loading', 'general', '', '0', '0', '');";
+					break;
+					case "subscription":
+						location.href = "javascript:loadBox('user/" + DeezerPlus.getUsername() + "/info', 'loading', 'general', '', '1', '0', '');";
+					break;
+					case "friends":
+						location.href = "javascript:loadBox('user/" + DeezerPlus.getUsername() + "/feed', 'loading', 'general', '', '1', '0', '');";
+					break;
+					case "logout":
+						location.href = "javascript:user.logout();";
+					break;
+					case "home":
+						location.href = "javascript:loadBox('index.php', 'loading', 'general', '', '1', '0', '');";
+					break;
+					case "inbox":
+						location.href = "javascript:loadBox('user/" + DeezerPlus.getUsername() + "/inbox/received/0', 'loading', 'general', '', '1', '0', '');";
+					break;
+				}
+		    break;
+        }
+     });
+
+document.addEventListener('load', DeezerPlus.onDomLoad , true);
+var timerDeezerPlus = setInterval("location.href='javascript:updateDeezerPlus();';",7500);
+
+//var timerDeezerPlus = setInterval("location.href = \"javascript:document.getElementById('deezerPlus').setAttribute('album',dzPlayer.albumTitle);document.getElementById('deezerPlus').setAttribute('playing',dzPlayer.isPlaying());\";", 333);
+
+
+interfaceTweaks.init('page');
