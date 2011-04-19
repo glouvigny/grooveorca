0848ad961cc50dd35467d9a45d105908ee3b4d27
Guillaume L <tuxkowo@gmail.com>
2011-04-19 20:08:54 +0200

    Update of resources scripts.

---
+++ CleanGrooveshark.cmd
@@ -0,0 +1,27 @@
+cd res\icons
+del Ext_icon*
+copy Grooveshark_icon_128x128.png Ext_icon_128x128.png
+copy Grooveshark_icon_16x16.png Ext_icon_16x16.png
+copy Grooveshark_icon_32x32.png Ext_icon_32x32.png
+copy Grooveshark_icon_48x48.png Ext_icon_48x48.png
+cd ..
+cd ..
+cd scripts
+del *Deezer.js
+cd ..
+cd res\icons
+del Deezer*
+cd ..
+cd ..
+cd res\icons\
+del Deezer*
+cd ..
+cd ..
+cd res\options\
+del Deezer*.png
+cd ..
+cd ..
+del *.txt
+del "LICENCE"
+del "README.md"
+del *.cmd
--- IconsDeezer.cmd
+++
@@ -1,6 +0,0 @@
-cd res\icons
-del Ext_icon*
-cp Deezer_icon_128x128.png Ext_icon_128x128.png
-cp Deezer_icon_16x16.png Ext_icon_16x16.png
-cp Deezer_icon_32x32.png Ext_icon_32x32.png
-cp Deezer_icon_48x48.png Ext_icon_48x48.png
