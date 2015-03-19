ab600649f8d81d52dbbdd29ccd743e118d0409e6
Guillaume Louvigny <glouvigny@users.noreply.github.com>
2015-03-19 21:03:02 +0100

    install.sh: cloning caoutchouc from github, for easier development

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 cd bin/
 mkdir -p ext/
 cd ext/
+rm -rf chr2moz/
 git clone https://github.com/Tuxkowo/chr2moz.git
 cd chr2moz/
 npm install
@@ -18,6 +19,8 @@ cd ../
 # install backend dependencies
 cd lib/
 bower install
+rm -rf ext/caoutchouc/
+git clone git@github.com:Tuxkowo/caoutchouc.git ext/caoutchouc/
 cd ext/caoutchouc/
 bower install
 cd ../../../
