8e79c01d387e612e8f51a2d66ebf2337ad5f0b0c
Guillaume Louvigny <glouvigny@users.noreply.github.com>
2015-03-21 23:50:55 +0100

    Grooveshark: shitty new version, wip

--- data/watchers/player/grooveshark.js
+++ data/watchers/player/grooveshark.js
@@ -16,6 +16,10 @@
         return window.GS || unsafeWindow.GS;
     };
 
+    var _gshark = function () {
+        return window.Grooveshark || unsafeWindow.Grooveshark;
+    };
+
     var bridgeWatcher = new BridgeWatcher();
     var updateData = bridgeWatcher.updateData.grooveOrcaBind(bridgeWatcher);
 
@@ -23,42 +27,36 @@
     var repeatModes = {'none': 0, 'song': 2, 'all': 1};
 
     var updateTrack =  function (force) {
-        if (!_gs().Services.SWF.getPlaybackStatus()) {
+        if (!_gshark().getCurrentSongStatus().song) {
             return;
         }
 
-        var raw = _gs().Services.SWF.getPlaybackStatus().activeSong;
+        var raw = _gshark().getCurrentSongStatus().song;
 
         if (!raw) {
             return;
         }
 
         updateData('song', {
-            artist: raw.ArtistName,
-            track: raw.SongName,
-            album: raw.AlbumName,
-            album_id: raw.AlbumName,
-            track_id: raw.SongID,
-            artist_id: raw.ArtistID,
-            duration: parseInt(raw.EstimateDuration, 10),
-            album_art: 'http://images.gs-cdn.net/static/albums/120_' +
-                raw.CoverArtFilename,
+            artist: raw.artistName,
+            track: raw.songName,
+            album: raw.albumName,
+            album_id: raw.albumName,
+            track_id: raw.songID,
+            artist_id: raw.artistID,
+            duration: parseInt(raw.estimateDuration, 10),
+            album_art: raw.artURL,
             disk_number: 0,
-            track_number: 0,
+            track_number: raw.trackNum,
         }, force);
     };
 
     var updatePosition = function (force) {
-        if (!_gs().Services.SWF.getPlaybackStatus()) {
-            return;
-        }
-
-        updateData('position', parseInt(_gs().Services.SWF.getPlaybackStatus()
-            .position / 1000, 10), force);
+        updateData('position', parseInt(0, 10), force); /* TODO */
     };
 
     var updateVolume = function (force) {
-        updateData('volume', _gs().Services.SWF.getVolume() / 100, force);
+        updateData('volume', _gshark().getVolume() / 100, force);
     };
 
     var updateState = function (force) {
@@ -74,16 +72,14 @@
     };
 
     var updateShuffle = function (force) {
-        updateData('shuffle', !!_gs().Services.SWF.getCurrentQueue()
-            .shuffleEnabled, force);
+        updateData('shuffle', false, force); /* TODO */
     };
 
     var updateRepeat = function (force) {
         var repeat = 'none';
         for (var i in repeatModes) {
             if (repeatModes.hasOwnProperty(i)) {
-                if (repeatModes[i] === _gs().Services.SWF.getCurrentQueue()
-                    .repeatMode) {
+                if (repeatModes[i] === 'none') { /* TODO */
                     repeat = i;
                 }
             }
@@ -102,38 +98,20 @@
     ];
 
     var actions = {
-        'playpause': function () { _gs().Services.SWF.togglePlay(); },
-        'next': function () { _gs().Services.SWF.nextSong(); },
-        'previous': function () { _gs().Services.SWF.previousSong(); },
+        'playpause': function () { _gshark().togglePlayPause(); },
+        'next': function () { _gshark().next(); },
+        'previous': function () { _gshark().previous(); },
         'volume': function (param) {
-            _gs().Services.SWF.setVolume(param.volume * 100);
+            _gshark().setVolume(param.volume);
         },
-        'play': function () { _gs().widgets.player.resume(); },
-        'pause': function () { _gs().Services.SWF.resumeSong(); },
+        'play': function () { _gshark().togglePlayPause(); /* TODO */ },
+        'pause': function () { _gshark().pause(); },
         'seek': function (param) {
-            _gs().Services.SWF.seekTo(param.position * 1000);
+            _gshark().seekTo(param.position * 1000);
         },
-        'repeat': function (param) {
-            if (param.mode === undefined) {
-                param.mode = nextRepeatMode[_gs().Services.SWF.getCurrentQueue()
-                    .repeatMode];
-            } else {
-                param.mode = repeatModes[param.mode];
-            }
-
-            if (param.mode !== undefined) {
-                _gs().Services.SWF.setRepeat(param.mode);
-            }
-        },
+        'repeat': function (param) { /* TODO */ },
         'update_track': function() { updateTrack(true); },
-        'shuffle': function (param) {
-            if (param.shuffle === undefined) {
-                param.shuffle = !_gs().Services.SWF.getCurrentQueue()
-                    .shuffleEnabled;
-            }
-
-            _gs().Services.SWF.setShuffle(param.shuffle);
-        },
+        'shuffle': function (param) { /* TODO */ },
     };
 
     bridgeWatcher.registerActions(actions);
